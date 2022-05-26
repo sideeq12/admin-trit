93dc282ea2d36e4ffe2d46e10a0aff9c3a69b04f
sideeq12 <abdwaheed2018@gmail.com>
2022-05-26 14:55:31 +0000

    details

---
+++ components/order_details/order.tsx
@@ -0,0 +1,10 @@
+import { Order_section } from "./orderdetail"
+
+
+export const Order_details = () =>{
+    return(
+        <Order_section>
+            hello testijng
+        </Order_section>
+    )
+}
