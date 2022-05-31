91b9450313023eaedea3e1f91937d9fd6ef82f78
sideeq12 <abdwaheed2018@gmail.com>
2022-05-31 10:26:49 +0000

    nav build

---
+++ .next/static/2AHTkr7B0DH_XOmOWuO9g/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/474-428ca3e4a4c97d2b.js","static/chunks/pages/index-92ecd0567e17876c.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
