327822764922cfaee1c3da4603a387b379c535a0
sideeq12 <abdwaheed2018@gmail.com>
2022-06-22 14:56:40 +0000

    layout

---
+++ pages/api/connection/models/productSchema.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose"
+
+const productSchema = new mongoose.Schema({
+    product_name : String,
+    Description : String,
+    price : String,
+    prevPrice : String, 
+    reviewPoint : String,
+    reviewCount : String,
+    categories : String,
+    size : String,
+    color : String,
+    productCount : String,
+    reviews : [
+        {
+            id : Number,
+            message : String,
+            sender : String
+        }
+    ]
+})
+
+const Product =mongoose.model("Products", productSchema)
+
+export default Product;
